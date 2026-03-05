862def031fd97d07a6055f7b854246b3ba4eb924
Lizzy-gt <luiz.gaspar@aluno.senai.br>
2026-03-05 13:52:52 -0300

    Adicionais

--- Exercicios/aula04/ex3.js
+++ Exercicios/aula04/ex3.js
@@ -1,8 +1,12 @@
 /* GABARITO 3 */
-let valor = prompt("Valor da compra:");
-let cupom = prompt("Tem cupom? (sim/nao)");
-if (valor > 150 || cupom == "sim") {
-    console.log("Frete Grátis Liberado");
-} else {
-    console.log("Frete Pago");
-}
+let valorCompra = prompt("Digite o valor da compra")
+// let valor = prompt("Valor da compra:");
+// let cupom = prompt("Tem cupom? (sim/nao)").toLowerCase().trim()
+// if (valor > 150 ||/*ou*/ cupom == "sim") {
+//     console.log("Frete Grátis Liberado");
+// } else {
+//     console.log("Frete Pago");
+// }
+
+let cupom = confirm("Se você possui cupom clique em 'Confimar', se não clique em 'Cancelar' ")
+let resposta = valorCompra > 150 || cupom == true ? "Tem frete pago" : "Não tem frete pago"
--- Exercicios/aula04/ex4.js
+++ Exercicios/aula04/ex4.js
@@ -1,6 +1,6 @@
 /* GABARITO 4 */
 let userNum = prompt("Escolha de 1 a 10:");
-let sortudo = Math.floor(Math.random() * 10) + 1;
+let sortudo = Math.floor(Math.random() /*Redondar para baixo, mas soamando paara cima*/ * 10) + 1;
 if (userNum == sortudo) {
     alert("Parabéns, você ganhou um brinde!");
 } else {
